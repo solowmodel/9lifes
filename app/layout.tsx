a42a9c087c5787beaff2a21d3eb44acfd83d71db
TimdinanCSUF <70355828+TimdinanCSUF@users.noreply.github.com>
2026-01-25 12:07:40 -0800

    9 lifes landing

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Pack Ratt Records & Vintage",
+  description: "Pack Ratt Records & Vintage, San Diego: Rare vinyl, vintage clothes, & unique finds! We are open, come hang!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
